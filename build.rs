bc48e2fe64e81d05e8984528f69ddb5c7a907e6f
AngryMane <regulationdango@gmail.com>
2025-06-18 09:55:47 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .build_server(false)
         .build_client(true)
         .compile(
